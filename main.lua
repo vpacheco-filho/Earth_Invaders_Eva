040f809aa737303c97dc1e42dfffc94a8bc911fb
sam.blacklotus <sam.blacklotus@gmail.com>
2023-12-03 21:57:57 -0300

    arquivos

--- main.lua
+++ main.lua
@@ -1,2 +1,17 @@
-print("Hello World!")
+function love.load()
+	local tela_de_jogo
 
+    tela_de_jogo = criarNave(200,400)
+
+end    
+
+
+function CriarNave (x, y)
+    local novaNave = {};
+
+    novaNave.imagem = love.graphics.newImage ('art/spacer.png');
+    novaNave.x = x;
+    novaNave.y = y;
+
+    return novaNave;
+end    
